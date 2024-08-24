3156bd8fbbd1655dd3f11eaf4c0a1c7186587a83
Simon Zuluaga <simozuluaga@gmail.com>
2024-08-24 17:16:54 -0500

    feat(arch): add basic heartbeat RPC

--- server/linker_server.go
+++ server/linker_server.go
@@ -2,19 +2,32 @@ package server
 
 import (
 	"context"
+	"fmt"
+	"time"
 
 	proto00 "github.com/sicozz/project00/api/v0.0"
 	"github.com/sicozz/project00/handler"
+	"github.com/sicozz/project00/utils"
 )
 
 type LinkerService struct {
 	proto00.UnimplementedLinkerServer
 }
 
-func (s LinkerService) Info(ctx context.Context, req *proto00.InfoReq) (res *proto00.InfoRes, err error) {
+func (s *LinkerService) Info(ctx context.Context, req *proto00.InfoReq) (res *proto00.InfoRes, err error) {
 	programInfo := handler.QueryGetServiceInfo()
 	return &proto00.InfoRes{
 		Version: programInfo.Version,
 		Banner:  programInfo.Banner,
 	}, nil
+}
+
+func (s *LinkerService) Subscribe(req *proto00.SubscribeReq, stream proto00.Linker_SubscribeServer) error {
+	term := 1000
+	for {
+		stream.Send(&proto00.Heartbeat{Term: fmt.Sprintf("%v", term)})
+		utils.Info(fmt.Sprintf("Sent heartbeat: %v", term))
+		time.Sleep(1 * time.Second)
+		term = term + 1
+	}
 }
