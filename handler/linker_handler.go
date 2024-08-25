9741c8c73373c8a5932fd69b92f5ec56281d9898
Simon Zuluaga <simozuluaga@gmail.com>
2024-08-25 17:23:26 -0500

    feat(arch): add handling of client disconnection on streaming RPC

--- handler/linker_handler.go
+++ handler/linker_handler.go
@@ -1,10 +1,33 @@
 package handler
 
 import (
+	"fmt"
+	"time"
+
+	proto00 "github.com/sicozz/project00/api/v0.0"
 	"github.com/sicozz/project00/datatype"
 	"github.com/sicozz/project00/utils"
 )
 
-func QueryGetServiceInfo() datatype.ProgramInfo {
-	return datatype.ProgramInfo{Version: utils.VERSION, Banner: utils.BANNER}
+func QueryGetServiceInfo() (*proto00.InfoRes, error) {
+	programInfo := datatype.ProgramInfo{Version: utils.VERSION, Banner: utils.BANNER}
+	return &proto00.InfoRes{
+		Version: programInfo.Version,
+		Banner:  programInfo.Banner,
+	}, nil
+}
+
+func HandleSubscription(stream proto00.Linker_SubscribeServer) error {
+	term := 1000
+	for {
+		select {
+		case <-stream.Context().Done():
+			return stream.Context().Err()
+		default:
+			stream.Send(&proto00.Heartbeat{Term: fmt.Sprintf("%v", term)})
+			utils.Info(fmt.Sprintf("Sent heartbeat: %v", term))
+			term = term + 1
+			time.Sleep(1 * time.Second)
+		}
+	}
 }
--- server/linker_server.go
+++ server/linker_server.go
@@ -3,7 +3,6 @@ package server
 import (
 	"context"
 	"fmt"
-	"time"
 
 	proto00 "github.com/sicozz/project00/api/v0.0"
 	"github.com/sicozz/project00/handler"
@@ -15,19 +14,12 @@ type LinkerService struct {
 }
 
 func (s *LinkerService) Info(ctx context.Context, req *proto00.InfoReq) (res *proto00.InfoRes, err error) {
-	programInfo := handler.QueryGetServiceInfo()
-	return &proto00.InfoRes{
-		Version: programInfo.Version,
-		Banner:  programInfo.Banner,
-	}, nil
+	return handler.QueryGetServiceInfo()
 }
 
 func (s *LinkerService) Subscribe(req *proto00.SubscribeReq, stream proto00.Linker_SubscribeServer) error {
-	term := 1000
-	for {
-		stream.Send(&proto00.Heartbeat{Term: fmt.Sprintf("%v", term)})
-		utils.Info(fmt.Sprintf("Sent heartbeat: %v", term))
-		time.Sleep(1 * time.Second)
-		term = term + 1
-	}
+	// TODO: Move this to the handler
+	err := handler.HandleSubscription(stream)
+	utils.Error(fmt.Sprintf("Follower unsubscribed: %v", err))
+	return err
 }
