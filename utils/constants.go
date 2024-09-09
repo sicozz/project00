4da8de45a7635074aae015944114f3244b657d9e
Simon Zuluaga <simozuluaga@gmail.com>
2024-09-08 19:19:19 -0500

    chore(docker): add static ips for distenv containers
    This is done to be able to use a static ip list for the hosts on
    hosts.txt

--- utils/constants.go
+++ utils/constants.go
@@ -1,8 +1,8 @@
 package utils
 
 const (
-	VERSION            = "0.0"
+	VERSION            = "0.0.0"
 	BANNER             = "PROJECT00"
-	DEFAULT_LOG_FILE   = "/tmp/project00"
+	DEFAULT_LOG_FILE   = "/tmp/project00.log"
 	DEFAULT_HOSTS_FILE = "hosts.txt"
 )
